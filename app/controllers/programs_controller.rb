1cbd5d347715c3e7571820cc6d9c50d7ebca8d5c
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2025-06-02 01:54:45 -0500

    re-factor workout view

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -4,7 +4,11 @@ class ProgramsController < ApplicationController
   end
 
   def show
-    demo_programs = Rails.application.config_for(:demo_programs)
-    @program = demo_programs[params[:id]] || demo_programs["1"]
+    demo_programs = Rails.application.config_for(:demo_programs)["programs"]
+    # debugger
+    Rails.logger.debug "Program: #{demo_programs.inspect}"
+    @program = demo_programs[params[:id]] || demo_programs[1]
+    # debugger
+    Rails.logger.debug "Program: #{@program.inspect}"
   end
 end
